03eb2218d3ebb9caa85ac5bb216a830ffafb49e0
Yudo <53389941+juniz@users.noreply.github.com>
2024-06-24 14:18:26 +0700

    comit

--- resources/js/Pages/Dashboard/Index.tsx
+++ resources/js/Pages/Dashboard/Index.tsx
@@ -1,10 +1,5 @@
 import { Head } from "@inertiajs/react";
-import { PageProps } from "@/types";
 import Login from "@/Layouts/LoginLayout";
-import FormMou from "./FormMou";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { DataMou } from "./DataMou";
-
 export default function Dashboard({ mou }: { mou: any }) {
   return (
     <Login>
@@ -13,7 +8,7 @@ export default function Dashboard({ mou }: { mou: any }) {
       <div className="py-8">
         <div className="container mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <Tabs defaultValue="input">
+            {/* <Tabs defaultValue="input">
               <TabsList>
                 <TabsTrigger value="input">Input</TabsTrigger>
                 <TabsTrigger value="data">Data</TabsTrigger>
@@ -24,7 +19,7 @@ export default function Dashboard({ mou }: { mou: any }) {
               <TabsContent value="data" className="m-4">
                 <DataMou mou={mou} />
               </TabsContent>
-            </Tabs>
+            </Tabs> */}
           </div>
         </div>
       </div>
